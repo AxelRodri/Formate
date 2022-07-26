8f74fd10a3e275f10cde892ecd1f593d7279a569
Facundo <cailloureloaded@outlook.com>
2022-07-26 16:05:36 -0300

    Creo que lo arregle

--- formate.js
+++
@@ -1,5 +0,0 @@
-const btnMenu = document.querySelector(".btnMenu");
-const enlaces = document.querySelector(".enlaces-menu");
-btnMenu.addEventListener("click", function(){
-    menu.classList.toggle("activado");
-})
